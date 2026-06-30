c5099c2d27ff07b16853cc062ca22e7e5bc606d6
Jamie Phillips <jamie.phillips915@gmail.com>
2026-06-30 03:55:31 +0100

    impoved responsiveness

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -20,7 +20,7 @@ function HomePage() {
                     <div style={{ width: "10%", height: "100%" }}></div>
                 </div>
 
-                <div style={{ width: "100%", height: "10vh", display: "flex", justifyContent: "center" }}>
+                <div className="watermarkDiv">
                     <p style={{color: "white", alignSelf: "center", fontSize: "0.5rem"}}>Made by Jamie Phillips</p>
                 </div>
             </div>
